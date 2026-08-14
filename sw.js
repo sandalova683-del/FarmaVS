e461843f031838abdcc6f97ac7359c8e65469957
Vera Sandalova <sandalova683sandalova683@gmail.com>
2026-08-14 09:34:13 +0300

    дневник дня

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_CACHE = 'formulavs-shell-v2.10.7';
-const APP_VERSION = '2.10.7';
+const APP_CACHE = 'formulavs-shell-v2.10.9';
+const APP_VERSION = '2.10.9';
 const CORE_ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.svg','./version.json'];
 
 self.addEventListener('install', event => {
