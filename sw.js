96dfb21b7b58191655ba2476c986b94599f0a70f
Vera Sandalova <sandalova683sandalova683@gmail.com>
2026-09-03 15:46:36 +0300

    исправлены объемы

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_CACHE = 'formulavs-shell-v2.13.0';
-const APP_VERSION = '2.13.0';
+const APP_CACHE = 'formulavs-shell-v2.13.1';
+const APP_VERSION = '2.13.1';
 const CORE_ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-48.png','./icon-180.png','./icon-192.png','./icon-512.png','./version.json'];
 
 self.addEventListener('install', event => {
